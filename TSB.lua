ac0cee95150b469c6434f4a73dcf08b0e5b85942
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-20 00:23:43 +0700

    Update TSB.lua

--- TSB.lua
+++ TSB.lua
@@ -5,7 +5,7 @@ local Window = Rayfield:CreateWindow({
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "DYHUB | dsc.gg/dyhub",
    LoadingSubtitle = "Powered by DYHUB TEAM",
-   ShowText = "gurt yo", -- for mobile users to unhide rayfield, change if you'd like
+   ShowText = "DYHUB - Open", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Dark Blue", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
