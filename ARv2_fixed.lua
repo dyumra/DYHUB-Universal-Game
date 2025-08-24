ed142364b4a3378f8fec7192b94b54be4d841cff
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-24 22:25:27 +0700

    Update ARv2_fixed.lua

--- ARv2_fixed.lua
+++ ARv2_fixed.lua
@@ -88,7 +88,7 @@ MainTab:Button({
             local unlocked = data:FindFirstChild(name) and data[name].Value
             if not unlocked then
                 event:FireServer("SetMorphBuy", name, 0)
-                task.wait(0.4)
+                task.wait(0.25)
             end
         end
 
