54d28a155a267134ade5b9e5ff35b73c5abbeb05
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-24 22:58:17 +0700

    Update ARv2_fixed.lua

--- ARv2_fixed.lua
+++ ARv2_fixed.lua
@@ -47,6 +47,7 @@ Window:EditOpenButton({
 })
 
 -- Tabs with Icons
+local InfoTab = Window:Tab({ Title = "Info", Icon = "info" })
 local MainTab = Window:Tab({ Title = "Main", Icon = "rocket" }) 
 local CashTab = Window:Tab({ Title = "Cash", Icon = "dollar-sign" }) 
 local PartyTab = Window:Tab({ Title = "Auto Join", Icon = "handshake" }) 
@@ -54,6 +55,23 @@ local GUI = Window:Tab({ Title = "Equip", Icon = "flame" })
 local GamepassTab = Window:Tab({ Title = "Gamepass", Icon = "cookie" }) 
 local PlayerTab = Window:Tab({ Title = "Player", Icon = "user" }) 
 local MiscTab = Window:Tab({ Title = "Misc", Icon = "file-cog" }) 
+
+Window:SelectTab(1)
+
+InfoTab:Section({ Title = "Info - Read before using this script", Icon = "info" })
+InfoTab:Section({ Title = "This script is still under development." })
+InfoTab:Section({ Title = "If there are any bugs or issues" })
+InfoTab:Section({ Title = "you can report them to us on Discord" })
+
+InfoTab:Button({
+    Title = "DYHUB - Discord!",
+    Callback = function()
+        setclipboard("https://dsc.gg/dyhub")
+    end
+})
+
+InfoTab:Section({ Title = "inf money & spin, if rejoin u will be reset.", Icon = "triangle-alert" })
+InfoTab:Section({ Title = "We appreciate your choice to use our script.", Icon = "heart" })
 
 -- ====== Main Tab ======
 
