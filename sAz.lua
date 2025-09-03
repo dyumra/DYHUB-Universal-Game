c76479e51eb8f051daff7374c59fed00c52b2489
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-09-03 23:20:04 +0700

    Create sAz.lua

---
+++ sAz.lua
@@ -0,0 +1,20 @@
+local Cash = getgenv().Cash or 0
+local OpenBestEgg = getgenv().Egg or false  
+
+local args2 = {
+    "EggZombie",
+    {
+        amount = -Cash
+    }
+}
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("PurchaseEvent"):FireServer(unpack(args2))
+
+if OpenBestEgg then
+    local bestEggArgs = {
+        "EggWarrior",
+        {
+            amount = 10
+        }
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("PurchaseEvent"):FireServer(unpack(bestEggArgs))
+end
